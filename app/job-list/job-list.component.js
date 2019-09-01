e5ab4a75b840d041575f38abb1ab48c49fccfcbe
xenobarbital <greencross.am@gmail.com>
2019-09-01 14:56:07 +0400

    WIP: filters and styles

--- app/job-list/job-list.component.js
+++ app/job-list/job-list.component.js
@@ -1,32 +1,14 @@
 angular.module('jobList').
   component('jobList', {
     templateUrl: '/app/job-list/job-list.template.html',
-    controller: function JobListController() {
-      this.jobs = [
-        {
-          'id': 101,
-          'company': 'Space-X',
-          'logo': '',
-          'position': 'Rocket scientist',
-          'location': 'Hawthorne, California, U.S.',
-          'fulltime': true
-        },
-        {
-          'id': 102,
-          'company': 'DELL',
-          'logo': '',
-          'position': 'Senior software developer',
-          'location': 'Round Rock, Texas, U.S.',
-          'fulltime': true
-        },
-        {
-          'id': 103,
-          'company': 'IBM',
-          'logo': '',
-          'position': 'System administrator',
-          'location': 'Armonk, New York, U.S.',
-          'fulltime': true
-        },
-      ];
-    }
+    controller: ['$http', function JobListController($http) {
+      const self = this;
+      self.orderProp = 'position';
+      self.employmentType = '';
+      $http.get('https://job-listings-67a80.firebaseio.com/openings.json')
+        .then(res => {
+          console.log(res.data);
+          self.jobs = res.data;
+        });
+    }]
   });
